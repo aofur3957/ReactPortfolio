4c3afec41e4b7114889a8c8c03b0b3692fb94079
aofur3957 <sunggin69@gmail.com>
2022-02-11 16:52:26 +0900

    메인페이지 디폴트 게시글 삽입

--- src/components/main/News.js
+++ src/components/main/News.js
@@ -1,17 +1,29 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 export default function News(){
+  //초기 로딩 시 사용자 컴퓨터에 localStorage에 데이터가 없을 시 임의로 보여줄 초기 데이터
+  const defaultData = [
+    {title: 'Hello1', content: 'Here comes description in detail.'},
+    {title: 'Hello2', content: 'Here comes description in detail.'},
+    {title: 'Hello3', content: 'Here comes description in detail.'},
+    {title: 'Hello4', content: 'Here comes description in detail.'}
+  ]
+
   const getLocalItems = ()=>{
     let data = localStorage.getItem('posts');
 
     if(data){
       return JSON.parse(data);
     }else{
-      return [];
+      return defaultData;
     }
   }
 
   const [posts] = useState(getLocalItems);
+
+  useEffect(()=>{
+    localStorage.setItem('posts', JSON.stringify(posts));
+  },[])
 
   return (
     <section id="News">
@@ -19,12 +31,14 @@ export default function News(){
         <h1>Recent Post</h1>
 
         {posts.map((post, idx)=>{
-          return (
-            <li key={idx}>
-              <h2>{post.title}</h2>
-              <p>{post.content}</p>
-            </li>
-          )
+          if(idx < 4) {
+            return (
+              <li key={idx}>
+                <h2>{post.title}</h2>
+                <p>{post.content}</p>
+              </li>
+            )
+          }
         })}
         
       </div>
